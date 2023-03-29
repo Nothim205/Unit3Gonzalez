ab1683e7cbe0e749777e51a9b91a26040240a523
Nothim205 <Najhimg@gmail.com>
2023-03-29 15:22:00 -0700

    3.2 p4

--- Unit3Gonzalez/Assets/Scripts/MoveLeft.cs
+++ Unit3Gonzalez/Assets/Scripts/MoveLeft.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MoveLeft : MonoBehaviour
 {
-    private float speed = 30;
+    private float speed = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +14,6 @@ public class MoveLeft : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * Time.deltTime * speed);
+        transform.Translate(Vector3.left * Time.deltaTime * speed);
     }
 }
--- Unit3Gonzalez/Assets/Scripts/SpawnManager.cs
+++ Unit3Gonzalez/Assets/Scripts/SpawnManager.cs
@@ -6,10 +6,19 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject obsticlePrefab;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
+
+    private float startDelay = 2;
+    private float repeatRate = 2;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+    }
+
+    void SpawnObstacle()
+    {
+        Instantiate(obsticlePrefab, spawnPos, obsticlePrefab.transform.rotation);
     }
 
     // Update is called once per frame
